141f197b6bc0709aaed086087aba07103d791c75
Fafruch <fafruszek@gmail.com>
2017-10-08 18:55:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React RRule Generator
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Rrule generator</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
