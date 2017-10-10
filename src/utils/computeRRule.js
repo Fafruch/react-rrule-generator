342043ed44ee509ebbd524c08c462238e4d3b4fb
Fafruch <fafruszek@gmail.com>
2017-10-10 11:41:29 +0200

    Generating ending rrule works, add rrule.js & moment.js dependencies

--- src/utils/computeRRule.js
+++ src/utils/computeRRule.js
@@ -1,3 +1,6 @@
+import RRule from 'rrule';
+import moment from 'moment';
+
 const computeRRule = ({
   repeatFrequency,
   repeatMode,
@@ -7,10 +10,21 @@ const computeRRule = ({
   onTheDay,
   onTheMonth,
   end,
-  endDate,
+  endAfter,
+  endOnDate,
 }) => {
-  // do some magic
-  return end + endDate;
+  const rruleObject = {};
+
+  if (end === 'After') {
+    rruleObject.count = endAfter;
+  }
+
+  if (end === 'On date') {
+    rruleObject.until = moment(endOnDate).format();
+  }
+
+  const rrule = new RRule(rruleObject);
+  return rrule.toString();
 };
 
 export default computeRRule;
