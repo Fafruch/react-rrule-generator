fdcb47f4872e5d4f711d78d844930801e5b302a8
Fafruch <fafruszek@gmail.com>
2017-10-17 14:54:00 +0200

    Move onto new boilerplate for publishing

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import './styles/index.css';
-import AppContainer from './components/AppContainer';
-
-const App = () => <AppContainer />;
-
-export default App;
---
+++ src/demo/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Example from '../lib';
+
+const App = () => (
+  <div>
+    <Example />
+  </div>
+);
+
+export default App;
