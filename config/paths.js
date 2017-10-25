e31d5c3ce9d8e4011350432f5b45194456db7aec
Fafruch <fafruszek@gmail.com>
2017-10-25 15:07:39 +0200

    Add webpack config for building app demo

--- config/paths.js
+++ config/paths.js
@@ -42,6 +42,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveApp('.env'),
   appBuild: resolveApp('build'),
+  appBuildDemo: resolveApp('build-demo'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appPackageJson: resolveApp('package.json'),
