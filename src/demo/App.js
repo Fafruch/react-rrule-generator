10dffb7e03ba88c4bd55075b848b0cc816f316c2
Fafruch <fafruszek@gmail.com>
2017-10-31 17:25:09 +0100

    Add Github stars, small view changes

--- src/demo/App.js
+++ src/demo/App.js
@@ -6,19 +6,38 @@ import githubLogo from './github_logo.png';
 
 const App = () => (
   <div>
+    <div className="app-navbar">
+      <a href="https://github.com/fafruch/react-rrule-generator">
+        &lt; go back to
+        {' '}
+        <img className="app-navbar-ghlogo" src={githubLogo} alt="Github logo" />
+        /fafruch
+      </a>
+
+      <iframe
+        title="github-star"
+        src="https://ghbtns.com/github-btn.html?user=fafruch&repo=react-rrule-generator&type=star&count=true&size=medium"
+        frameBorder="0"
+        scrolling="0"
+        width="72px"
+        height="20px"
+      />
+    </div>
     <div className="app-header">
-      <div className="app-header-github">
-        <a href="https://github.com/fafruch/react-rrule-generator">
-          go back to
-          {' '}
-          <img className="app-header-github-logo" src={githubLogo} alt="Github logo" />
-          /fafruch
-        </a>
-      </div>
       <h1>React RRule Generator</h1>
     </div>
+
+    <div className="app-desc">
+      Recurence rules generator form built with React
+    </div>
+
     <div className="app">
-      <ReactRRuleGenerator />
+      <ReactRRuleGenerator
+        config={{
+          yearly: 'on the',
+          end: ['On date'],
+        }}
+      />
     </div>
   </div>
 );
