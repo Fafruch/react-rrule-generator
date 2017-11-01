197fd5f1ef5512ac8ce6e54332b7cbdd0061f101
Fafruch <fafruszek@gmail.com>
2017-11-01 13:23:47 +0100

    Fix removed accidentally 50% opacity of not active modes

--- src/lib/components/Repeat/Monthly/OnThe.js
+++ src/lib/components/Repeat/Monthly/OnThe.js
@@ -12,7 +12,7 @@ const RepeatMonthlyOnThe = ({
   const isActive = mode === 'on the';
 
   return (
-    <div className="form-group row d-flex align-items-sm-center">
+    <div className={`form-group row d-flex align-items-sm-center ${!isActive && 'opacity-50'}`}>
       <div className="col-sm-1 offset-sm-2">
         {hasMoreModes && (
           <input
