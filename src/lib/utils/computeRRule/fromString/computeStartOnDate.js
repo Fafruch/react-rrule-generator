d6021daf2976ac9eead1b53f3805ba771d1e156f
Brent Christensen <brent@eventespresso.com>
2018-08-01 12:13:46 -0700

    add computeStartOnDate function for parsing dtstart value from rruleObj

---
+++ src/lib/utils/computeRRule/fromString/computeStartOnDate.js
@@ -0,0 +1,7 @@
+const computeStartOnDate = (data, rruleObj) => {
+  if (!rruleObj.dtstart) {
+    return data.start.onDate.date;
+  }
+  return rruleObj.dtstart;
+};
+export default computeStartOnDate;
