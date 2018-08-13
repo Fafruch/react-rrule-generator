5cd93b59b96d2ac9a04571b3dd8fb963fcdef05a
Brent Christensen <brent@eventespresso.com>
2018-08-13 14:36:36 -0700

    add id prop and pass to StartOnDate

--- src/lib/components/Start/index.js
+++ src/lib/components/Start/index.js
@@ -3,29 +3,33 @@ import PropTypes from 'prop-types';
 import StartOnDate from './OnDate';
 
 const Start = ({
+  id,
   start: {
     onDate,
   },
   handleChange,
-}) => (
-  <div className="px-3">
-    <div className="form-group row">
-      <div className="col-sm-2 text-sm-right">
-        <label
-          htmlFor="Start"
-          className="col-form-label"
-        >
-          <strong>
-            Start
-          </strong>
-        </label>
+}) => {
+  return (
+    <div className="px-3">
+      <div className="form-group row">
+        <div className="col-sm-2 text-sm-right">
+          <label
+            htmlFor={id}
+            className="col-form-label"
+          >
+            <strong>
+              Start
+            </strong>
+          </label>
+        </div>
+        <StartOnDate id={id} onDate={onDate} handleChange={handleChange} />
       </div>
-      <StartOnDate onDate={onDate} handleChange={handleChange} />
     </div>
-  </div>
-);
+  );
+};
 
 Start.propTypes = {
+  id: PropTypes.string.isRequired,
   start: PropTypes.shape({
     onDate: PropTypes.object.isRequired,
   }).isRequired,
