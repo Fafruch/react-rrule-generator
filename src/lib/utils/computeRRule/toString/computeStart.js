02b1d81f74ace168c7a167659f1ab685484b1748
Brent Christensen <brent@eventespresso.com>
2018-08-13 13:41:49 -0700

    set time to noon for new Dates to protect against timezone transitions such as DST to standard time

--- src/lib/utils/computeRRule/toString/computeStart.js
+++ src/lib/utils/computeRRule/toString/computeStart.js
@@ -5,11 +5,16 @@ const computeStart = ({ onDate: { date } }) => {
   // by seeing if it can be converted into a moment object.
   // if not, then create a new date
   if (!moment.isMoment(moment(date))) {
-    date = new Date().setMilliseconds(0);
+    date = new Date();
   }
 
   return {
-    dtstart: moment(date).toDate(),
+    dtstart: moment( date ).
+      hours( 12 ).
+      minutes( 0 ).
+      seconds( 0 ).
+      milliseconds( 0 ).
+      toDate()
   };
 };
 
