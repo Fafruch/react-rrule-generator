e8fc2fa77b5069e5c03a716b3f9fd12faa3fcaee
Brent Christensen <brent@eventespresso.com>
2018-08-13 14:45:06 -0700

    add id prop and use for input

--- src/lib/components/End/After.js
+++ src/lib/components/End/After.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import numericalFieldHandler from '../../utils/numericalFieldHandler';
 
 const EndAfter = ({
+  id,
   after,
   handleChange,
 }) => (
@@ -10,6 +11,7 @@ const EndAfter = ({
     <div className="form-group m-0 row d-flex align-items-center">
       <div className="col-3 col-sm-6 pl-0">
         <input
+          id={id}
           name="end.after"
           aria-label="End after"
           className="form-control"
@@ -25,6 +27,7 @@ const EndAfter = ({
 );
 
 EndAfter.propTypes = {
+  id: PropTypes.string.isRequired,
   after: PropTypes.number.isRequired,
   handleChange: PropTypes.func.isRequired,
 };
